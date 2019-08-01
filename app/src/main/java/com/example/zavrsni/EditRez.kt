dc380aacfd354e948b0f9b9c74b5df5d2307376f
Milwahr <milic.roberto@gmail.com>
2019-08-01 10:47:58 +0200

    Added: Implemented a better solution for editing the reservations

---
+++ app/src/main/java/com/example/zavrsni/EditRez.kt
@@ -0,0 +1,90 @@
+package com.example.zavrsni
+
+import android.app.DatePickerDialog
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_edit_rez.*
+import java.util.*
+
+class EditRez : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_rez)
+
+        intent = intent
+
+        editovanNaziv.setText(intent.getStringExtra("prenosIme"))
+        editovanDatumDol.setText(intent.getStringExtra("prenosDOL"))
+        editovanDatumOdl.setText(intent.getStringExtra("prenosODL"))
+        val prenosID = intent.getStringExtra("prenosID")
+
+        editovanDatumDol.setOnClickListener(){
+            prikDatumDol()
+        }
+        editovanDatumOdl.setOnClickListener(){
+            prikDatumOdl()
+        }
+
+        editSpremuj.setOnClickListener(){
+                val builder = AlertDialog.Builder(this)
+                    .setTitle("Obavijest")
+                    .setIcon(R.drawable.ic_warning_black_24dp)
+                    .setMessage("Da li ste sigurni da zelite urediti podatke?")
+                    .setPositiveButton("Da", DialogInterface.OnClickListener{dialog, which ->
+                        val isUpdate = MainActivity.dbHandler.editRez(prenosID, editovanNaziv.text.toString(), editovanDatumDol.text.toString(),
+                            editovanDatumOdl.text.toString())
+                        if(isUpdate == true) {
+                            Toast.makeText(this, "Uspjesno uredjena rezervacija ${editovanNaziv.text}", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }else{
+                            Toast.makeText(this, "Greska u uredjivanju", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                    .setNegativeButton("Ne", null)
+
+                    val alert = builder.create()
+                    alert.show()
+        }
+
+        editOdustanuj.setOnClickListener(){
+            finish()
+        }
+    }
+    /*
+    @SuppressLint("SetTextI18n")
+    fun prikDatumDol(){
+        val c = Calendar.getInstance()
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val month = c.get(Calendar.MONTH)
+        val year = c.get(Calendar.YEAR)
+
+        val dpd = DatePickerDialog(this, android.R.style.Theme_Holo_Dialog, DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+        txtnovidatumdol.setText("$day/$month/$year")}, year, month, day)
+        dpd.show()
+    }
+     */
+    fun prikDatumDol(){
+        val c = Calendar.getInstance()
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val month = c.get(Calendar.MONTH)
+        val year = c.get(Calendar.YEAR)
+
+        val dpd = DatePickerDialog(this, android.R.style.Theme_Holo_Dialog, DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+            editovanDatumDol.setText("$day/$month/$year")}, year, month, day)
+        dpd.show()
+    }
+    fun prikDatumOdl(){
+        val c = Calendar.getInstance()
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val month = c.get(Calendar.MONTH)
+        val year = c.get(Calendar.YEAR)
+
+        val dpd = DatePickerDialog(this, android.R.style.Theme_Holo_Dialog, DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+            editovanDatumOdl.setText("$day/$month/$year")}, year, month, day)
+        dpd.show()
+    }
+}
--- app/src/main/java/com/example/zavrsni/RezAdapter.kt
+++ app/src/main/java/com/example/zavrsni/RezAdapter.kt
@@ -5,22 +5,29 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.editrez.view.*
 import kotlinx.android.synthetic.main.novi_prikaz_rez.view.*
 import org.w3c.dom.Text
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 import java.util.*
 import kotlin.collections.ArrayList
 
-class RezAdapter(context: Context, val rezerv: ArrayList<Rezervacije>): RecyclerView.Adapter<RezAdapter.ViewHolder>(){
+class RezAdapter(context: Context, val rezerv: ArrayList<Rezervacije>): RecyclerView.Adapter<RezAdapter.ViewHolder>() {
 
     val context = context
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nazivRez = itemView.rezNaziv
         val datumRez = itemView.rezDatum
         val rezDelete = itemView.deleteRez
@@ -36,34 +43,36 @@ class RezAdapter(context: Context, val rezerv: ArrayList<Rezervacije>): Recycler
         return rezerv.size
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val rezervator : Rezervacije = rezerv[position]
+        val rezervator: Rezervacije = rezerv[position]
         holder.nazivRez.text = rezervator.imeRez
         holder.datumRez.text = "${rezervator.datumDOL} - ${rezervator.datumODL}"
 
-        holder.rezDelete.setOnClickListener(){
+        holder.rezDelete.setOnClickListener() {
             val rezervacijaIme = rezervator.imeRez
 
             var alertDialog = AlertDialog.Builder(context)
                 .setTitle("Upozorenje!")
                 .setMessage("Jeste li sigurni da biste obrisali rezervaciju $rezervacijaIme?")
-                .setPositiveButton("Da", DialogInterface.OnClickListener{dialog, which ->
-                    if(MainActivity.dbHandler.deleteRez(rezervator.idRez)){
+                .setPositiveButton("Da", DialogInterface.OnClickListener { dialog, which ->
+                    if (MainActivity.dbHandler.deleteRez(rezervator.idRez)) {
                         rezerv.removeAt(position)
                         notifyItemRemoved(position)
                         notifyItemRangeChanged(position, rezerv.size)
                         Toast.makeText(context, "Obrisana je rezervacija $rezervacijaIme", Toast.LENGTH_SHORT).show()
-                    }else
+                    } else
                         Toast.makeText(context, "Greska brisanja $rezervacijaIme", Toast.LENGTH_SHORT).show()
                 })
-                .setNegativeButton("Ne", DialogInterface.OnClickListener{dialog, which ->})
+                .setNegativeButton("Ne", DialogInterface.OnClickListener { dialog, which -> })
                 .setIcon(R.drawable.ic_warning_black_24dp)
                 .show()
         }
 
-        holder.rezEdit.setOnClickListener(){
-            val inflater = LayoutInflater.from(context)
+        holder.rezEdit.setOnClickListener() {
+            /*val inflater = LayoutInflater.from(context)
             val view = inflater.inflate(R.layout.editrez, null)
+
 
             val noviRezNaziv : TextView = view.findViewById(R.id.noviRezNaziv)
             val noviDatumDol : TextView = view.findViewById(R.id.noviDatumDol)
@@ -72,11 +81,23 @@ class RezAdapter(context: Context, val rezerv: ArrayList<Rezervacije>): Recycler
             noviRezNaziv.text = rezervator.imeRez
             noviDatumDol.text = rezervator.datumDOL
             noviDatumOdl.text = rezervator.datumODL
+*/
+            val prenosIme = rezervator.imeRez
+            val prenosDOL = rezervator.datumDOL
+            val prenosODL = rezervator.datumODL
+            val prenosID = rezervator.idRez.toString()
 
-            val builder = AlertDialog.Builder(context)
+            val intent = Intent(context, EditRez::class.java)
+            intent.putExtra("prenosIme", prenosIme)
+            intent.putExtra("prenosDOL", prenosDOL)
+            intent.putExtra("prenosODL", prenosODL)
+            intent.putExtra("prenosID", prenosID)
+            context.startActivity(intent)
+
+            /*val builder = AlertDialog.Builder(context)
                 .setTitle("Uredi Rezervaciju")
                 .setView(view)
-                .setPositiveButton("Uredi", DialogInterface.OnClickListener{dialog, which ->
+                .setPositiveButton("Uredu", DialogInterface.OnClickListener{ dialog, which ->
                     val isUpdate = MainActivity.dbHandler.editRez(rezervator.idRez.toString(), view.noviRezNaziv.text.toString(),
                         view.noviDatumDol.text.toString(), view.noviDatumOdl.text.toString())
                     if(isUpdate == true){
@@ -92,7 +113,7 @@ class RezAdapter(context: Context, val rezerv: ArrayList<Rezervacije>): Recycler
                 .setNegativeButton("Odustani", DialogInterface.OnClickListener{dialog, which ->})
             val alert = builder.create()
             alert.show()
+            }*/
         }
     }
-
 }
